a91e24721b2350e36ac6eda4795c0916cc3e6546
Michal Rotko <michalr835@gmail.com>
2024-12-22 14:02:05 +0100

    add auth to config-server

--- config-server/src/main/java/pl/rotkom/friday/config/FridayConfigApp.java
+++ config-server/src/main/java/pl/rotkom/friday/config/FridayConfigApp.java
@@ -3,12 +3,29 @@ package pl.rotkom.friday.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableWebSecurity
 public class FridayConfigApp {
 
     public static void main(String[] args) {
         SpringApplication.run(FridayConfigApp.class, args);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
     }
 }
